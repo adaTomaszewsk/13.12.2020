ea846199cb7ec85550cb38468c4a367b5d7d63f9
gpatrycja <p.golawska@gmail.com>
2020-08-23 18:48:40 +0200

    add new css/html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,22 +8,24 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { OrderComponent } from './order/order.component';
 import { MenuComponent } from './menu/menu.component';
 import { SupplierOrdersComponent } from './supplier-orders/supplier-orders.component';
 import { CustomerOrdersComponent } from './customer-orders/customer-orders.component';
 import { UndeliveredOrdersComponent } from './undelivered-orders/undelivered-orders.component';
+import { BasketComponent } from './basket/basket.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
     SupplierComponent,
-    OrderComponent,
     MenuComponent,
     SupplierOrdersComponent,
     CustomerOrdersComponent,
     UndeliveredOrdersComponent,
+    BasketComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,9 +2,9 @@ import {Component} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {first} from 'rxjs/operators';
-import {LoginService} from '../services/login.service';
-import {LoginInterface} from './login.interface';
-import {Role} from '../models';
+// import {LoginService} from '../services/login.service';
+// import {LoginInterface} from './login.interface';
+// import {Role} from '../models';
 
 @Component({
   selector: 'app-login',
@@ -43,73 +43,87 @@ export class LoginComponent {
   //   }
   // ]
 
-  loginForm: FormGroup;
-  testForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string;
-  error = '';
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private route: ActivatedRoute,
-    private router: Router,
-    private authenticationService: LoginService
-  ) {
-    // redirect to home if already logged in
-    if (this.authenticationService.currentUserValue) {
-      this.router.navigate(['/']);
-    }
-  }
+  constructor() { }
 
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    });
 
-    this.testForm = new FormGroup({
-      test: new FormControl(null)
-    });
 
-    this.testForm.get('test').valueChanges.subscribe(res => console.log(res));
 
-    // get return url from route parameters or default to '/'
-    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-  }
 
-  // convenience getter for easy access to form fields
-  get f() {
-    return this.loginForm.controls;
-  }
 
-  onSubmit() {
-    this.submitted = true;
 
-    // stop here if form is invalid
-    if (this.loginForm.invalid) {
-      return;
-    }
 
-    this.loading = true;
-    this.authenticationService.login(this.f.email.value, this.f.password.value)
-      .pipe(first())
-      .subscribe(
-        (user: LoginInterface) => {
-          this.submitted = false;
-          this.loading = false;
-          if (user.role === Role.Doctor) {
-            this.router.navigate(['lekarz']);
-          } else if (user.role === Role.Patient) {
-            this.router.navigate(['pacjent']);
-          } else {
-            this.router.navigate(['']);
-          }
-        },
-        error => {
-          this.error = error;
-          this.submitted = false;
-          this.loading = false;
-        });
-  }
+
+
+
+  // good
+  // loginForm: FormGroup;
+  // testForm: FormGroup;
+  // loading = false;
+  // submitted = false;
+  // returnUrl: string;
+  // error = '';
+  //
+  // constructor(
+  //   private formBuilder: FormBuilder,
+  //   private route: ActivatedRoute,
+  //   private router: Router,
+  //   private authenticationService: LoginService
+  // ) {
+  //   // redirect to home if already logged in
+  //   if (this.authenticationService.currentUserValue) {
+  //     this.router.navigate(['/']);
+  //   }
+  // }
+  //
+  // ngOnInit() {
+  //   this.loginForm = this.formBuilder.group({
+  //     email: ['', Validators.required],
+  //     password: ['', Validators.required]
+  //   });
+  //
+  //   this.testForm = new FormGroup({
+  //     test: new FormControl(null)
+  //   });
+  //
+  //   this.testForm.get('test').valueChanges.subscribe(res => console.log(res));
+  //
+  //   // get return url from route parameters or default to '/'
+  //   this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  // }
+  //
+  // // convenience getter for easy access to form fields
+  // get f() {
+  //   return this.loginForm.controls;
+  // }
+  //
+  // onSubmit() {
+  //   this.submitted = true;
+  //
+  //   // stop here if form is invalid
+  //   if (this.loginForm.invalid) {
+  //     return;
+  //   }
+  //
+  //   this.loading = true;
+  //   this.authenticationService.login(this.f.email.value, this.f.password.value)
+  //     .pipe(first())
+  //     .subscribe(
+  //       (user: LoginInterface) => {
+  //         this.submitted = false;
+  //         this.loading = false;
+  //         if (user.role === Role.Doctor) {
+  //           this.router.navigate(['lekarz']);
+  //         } else if (user.role === Role.Patient) {
+  //           this.router.navigate(['pacjent']);
+  //         } else {
+  //           this.router.navigate(['']);
+  //         }
+  //       },
+  //       error => {
+  //         this.error = error;
+  //         this.submitted = false;
+  //         this.loading = false;
+  //       });
+  // }
 }
